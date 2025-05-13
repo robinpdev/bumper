0a13eebf7d64db7a2b4be3440cf6aa277695f0f4
robinpdev <abcd@efg.com>
2025-05-13 15:49:37 +0200

    platform dependent make (temp fix lol)

--- makemodules.sh
+++ makemodules.sh
@@ -1,1 +1,8 @@
-make macmodule
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     make module;;
+    Darwin*)    make macmodule;;
+    MINGW*)     echo "coming soonr";;
+    MSYS_NT*)   echo "tf u doin!";;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
